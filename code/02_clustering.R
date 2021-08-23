8fe586db5ae5ffca58f54c989970a7e70c4b7068
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-08-23 11:10:48 +0200

    make clustering parallel

--- code/02_clustering.R
+++ code/02_clustering.R
@@ -1,4 +1,5 @@
 library("mclust")
+library("future")
 library("recipes")
 
 
@@ -12,14 +13,33 @@ rec = recipes::step_normalize(rec, all_numeric())
 estimates = recipes::prep(rec, training = data, retain = FALSE)
 saveRDS(estimates, "transformator.rds")
 data_trans = recipes::bake(estimates, data, composition = "data.frame")
-rm(rec)
+rm(data, rec)
 
-# clustering
-set.seed(123)
-mdl = mclust::Mclust(data_trans, G = 10:20)
-summary(mdl)
+# parallel clustering
+clusters = 12:20 # specify the number of clusters
+ncores = parallelly::availableCores(omit = 1)
+if (length(clusters) < ncores) ncores = length(clusters)
+future::plan(multisession, workers = ncores)
 
-# reduce model size
-mdl$data = mdl$data[0, ] # remove original data from model
-mdl$classification = as.integer(mdl$classification)
-saveRDS(mdl, "GMM_model.rds")
+# define parallel function in new environment
+mclust_par = local(function(data, G) {
+  mdl = Mclust(data = data, G = G)
+  # reduce model size
+  mdl$data = mdl$data[0, ] # remove original data
+  mdl$z = mdl$z[0, ] # remove probability
+  mdl$classification = as.integer(mdl$classification)
+  return(mdl)
+})
+
+results = vector("list", length(clusters))
+for (i in seq_along(clusters)) {
+  results[[i]] = future::future(mclust_par(data_trans, clusters[i]),
+                                seed = 123L)
+}
+results = future::value(results)
+
+# select the best model
+best = which.max(sapply(results, function(x) {x$bic}))
+results = results[[best]]
+summary(results)
+saveRDS(results, "GMM_model.rds")
