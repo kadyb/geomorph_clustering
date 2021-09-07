103cb535b5bba4dd0b11656693bff7f4b888e23e
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-09-07 17:17:30 +0200

    `spatial_predict()` now also returns uncertainty

--- code/utils/spatial_predict.R
+++ code/utils/spatial_predict.R
@@ -3,7 +3,7 @@ spatial_predict = function(x, transformator, mdl) {
   df = as.data.frame(x)
   xy = df[, c(1:2)]
   df = df[, -c(1:2)]
-  NA_found = any(is.na(df))
+  NA_found = anyNA(df)
 
   if (NA_found) {
     na_ids = which(is.na(df), arr.ind = TRUE)
@@ -11,20 +11,28 @@ spatial_predict = function(x, transformator, mdl) {
     df = df[-na_ids, ]
   }
 
-  pred = recipes::bake(transformator, df, composition = "data.frame")
+  df = recipes::bake(transformator, df, composition = "data.frame")
+  pred = predict(mdl, df) # TODO: make it parallel
   rm(df)
-  pred = predict(mdl, pred)$classification # TODO: make it parallel
+  uncertainty = numeric(nrow(pred$z))
+  for (i in seq_along(uncertainty)) {
+    uncertainty[i] = 1 - max(pred$z[i, ])
+  }
+  pred = pred$classification
 
   if (NA_found) {
-    full_vec = rep(NA_integer_, prod(dim(x)))
-    full_vec[-na_ids] = pred
-    pred = cbind(xy, full_vec)
+    full_pred = rep(NA_integer_, prod(dim(x)))
+    full_uncertainty = rep(NA_real_, prod(dim(x)))
+    full_pred[-na_ids] = pred
+    full_uncertainty[-na_ids] = uncertainty
+    raster = cbind(xy, full_pred, full_uncertainty)
   } else {
-    pred = cbind(xy, pred)
+    raster = cbind(xy, pred, uncertainty)
   }
 
-  pred = st_as_stars(pred, dims = c("x", "y"))
-  st_crs(pred) = st_crs(x)
-  return(pred)
+  raster = st_as_stars(raster, dims = c("x", "y"))
+  st_crs(raster) = st_crs(x)
+  names(raster) = c("cluster", "uncertainty")
+  return(raster)
 
 }
