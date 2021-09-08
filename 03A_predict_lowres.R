8b2947b7dc3269e78d0753f370b6c19ee21c4535
Krzysztof Dyba <35004826+kadyb@users.noreply.github.com>
2021-09-08 13:46:11 +0200

    explicitly call functions from packages

---
+++ 03A_predict_lowres.R
@@ -0,0 +1,36 @@
+library("sf")
+library("stars")
+library("mclust")
+library("recipes")
+source("code/utils/spatial_predict.R")
+
+
+ras_path = list.files("rasters", pattern = "\\.tif$", full.names = TRUE)
+varnames = basename(ras_path)
+varnames = substr(varnames, 1, nchar(varnames) - 4)
+varnames = substr(varnames, 4, nchar(varnames))
+varnames = tolower(varnames)
+
+# create virtual raster with lower resolution
+tmp = tempfile(fileext = ".vrt")
+resolution = c("-tr", 1000, 1000)
+resample = c("-r", "nearest")
+sf::gdal_utils(util = "buildvrt", source = ras_path, destination = tmp,
+               options = c(resolution, resample, "-separate"))
+
+rasters = stars::read_stars(tmp, proxy = FALSE)
+rasters = stars::st_set_dimensions(rasters, 3, values = varnames, names = "var")
+rasters = split(rasters)
+
+mdl = readRDS("GMM_model.rds")
+transformator = readRDS("transformator.rds")
+
+result = spatial_predict(rasters, transformator, mdl)
+
+plot(result["cluster"], col = sf.colors(n = mdl$G, categorical = TRUE))
+plot(result["uncertainty"], breaks = "equal")
+
+stars::write_stars(result["cluster"], "cluster.tif", options = "COMPRESS=LZW",
+                   type = "Byte", NA_value = 0)
+stars::write_stars(result["uncertainty"], "uncertainty.tif",
+                   options = "COMPRESS=LZW", type = "Float32", NA_value = 999)
---
+++ utils/spatial_predict.R
@@ -0,0 +1,38 @@
+spatial_predict = function(x, transformator, mdl) {
+
+  df = as.data.frame(x)
+  xy = df[, c(1:2)]
+  df = df[, -c(1:2)]
+  NA_found = anyNA(df)
+
+  if (NA_found) {
+    na_ids = which(is.na(df), arr.ind = TRUE)
+    na_ids = unique(na_ids[, 1])
+    df = df[-na_ids, ]
+  }
+
+  df = recipes::bake(transformator, df, composition = "data.frame")
+  pred = predict(mdl, df) # TODO: make it parallel
+  rm(df)
+  uncertainty = numeric(nrow(pred$z))
+  for (i in seq_along(uncertainty)) {
+    uncertainty[i] = 1 - max(pred$z[i, ])
+  }
+  pred = pred$classification
+
+  if (NA_found) {
+    full_pred = rep(NA_integer_, prod(dim(x)))
+    full_uncertainty = rep(NA_real_, prod(dim(x)))
+    full_pred[-na_ids] = pred
+    full_uncertainty[-na_ids] = uncertainty
+    raster = cbind(xy, full_pred, full_uncertainty)
+  } else {
+    raster = cbind(xy, pred, uncertainty)
+  }
+
+  raster = stars::st_as_stars(raster, dims = c("x", "y"))
+  sf::st_crs(raster) = sf::st_crs(x)
+  names(raster) = c("cluster", "uncertainty")
+  return(raster)
+
+}
